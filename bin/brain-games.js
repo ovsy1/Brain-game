bf59e4d144aee02c7382796322f09b509f61ba7b
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-01 04:18:31 +0300

    modified file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable import/no-unresolved */
-
-// eslint-disable-next-line import/extensions
-import { greeting } from 'frontent-project-lvl1/src/cli.js';
+/* eslint-disable import/extensions */
+import greeting from '../src/cli.js';
 
 greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-console */
-import readineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+
+export default greeting;

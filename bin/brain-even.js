165fe1d24f1fb4ef288268f078e771abdcff5fdd
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-01 04:19:49 +0300

    add executable file brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable import/extensions */
+import isEvenGame from '../src/even.js';
+
+isEvenGame();

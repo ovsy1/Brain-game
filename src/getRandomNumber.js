05f44d56ebfc07551fa841aeb9c7ce1f363b2edb
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-03 14:25:52 +0300

    add separate file with get random number func

---
+++ src/getRandomNumber.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+const getRandomNumber = (minNum, maxNum) => {
+  const min = Math.ceil(minNum);
+  const max = Math.floor(maxNum);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default getRandomNumber;

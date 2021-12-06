c4355ab9365358fe28f321e78a489ea4a96a08b9
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-06 14:58:59 +0300

    add new game logic

---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+import getRandomNumber from '../getRandomNumber.js';
+
+const rule = 'What number is missing in the progression?';
+
+const generatingProgressing = (startNum, stepProgression, progressionLength) => {
+  const arrayOfProgression = [];
+  for (let i = 0; i <= progressionLength; i += 1) {
+    arrayOfProgression.push(startNum + (stepProgression * i));
+  }
+  return arrayOfProgression;
+};
+
+const progressionGame = () => {
+  const firstNumber = getRandomNumber(1, 10);
+  const randomStep = getRandomNumber(1, 10);
+  const progressionLength = getRandomNumber(5, 10);
+  const progression = generatingProgressing(firstNumber, randomStep, progressionLength);
+  const randomIndex = getRandomNumber(0, progressionLength);
+  const correctAnswer = String(progression[randomIndex]);
+  progression[randomIndex] = '..';
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+};
+
+export { rule, progressionGame };

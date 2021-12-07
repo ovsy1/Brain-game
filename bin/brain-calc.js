1787c60f2c12da18c0238871666be25a8d547be5
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-07 11:27:39 +0300

    make one import

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 /* eslint-disable linebreak-style */
-import { rule, calculatorGame } from '../src/games/calc.js';
-import startBrainGame from '../src/index.js';
+import startGameCalculator from '../src/games/calc.js';
 
-startBrainGame(rule, calculatorGame);
+startGameCalculator();

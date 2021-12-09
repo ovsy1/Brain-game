94c892a0d40e6c262f7e954f4b0838aa15cb0e44
Ratmir Kalimulin <kalimulin.91@bk.ru>
2021-12-09 13:39:35 +0300

    rename function 'numIsEven' to 'isEven'

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,13 @@
-/* eslint-disable linebreak-style */
 import getRandomNumber from '../getRandomNumber.js';
 import startBrainGame from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const numIsEven = (num) => num % 2 === 0;
+const IsEven = (num) => num % 2 === 0;
 
 const isEvenGame = () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = numIsEven(question) ? 'yes' : 'no';
+  const correctAnswer = IsEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
